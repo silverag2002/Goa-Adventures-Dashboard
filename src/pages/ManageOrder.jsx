c97d7cf6a16d55c66490e437e16ac8969fa5dc65
Ajit Sharma <ajitsharma2021@gmail.com>
2023-06-13 21:51:51 +0530

    Order Page Added

--- src/pages/ManageOrder.jsx
+++ src/pages/ManageOrder.jsx
@@ -1,7 +1,154 @@
-import React from "react";
+import React, { useState } from "react";
+import { Box, useTheme } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+import { useGetTransactionsQuery } from "state/api";
+import Header from "components/Header";
+import DataGridCustomToolbar from "components/DataGridCustomToolbar";
+import { AiOutlineFilePdf } from "react-icons/ai";
 
 const ManageOrder = () => {
-  return <div>ManageOrder</div>;
+  const theme = useTheme();
+
+  // values to be sent to the backend
+  const [page, setPage] = useState(0);
+  const [pageSize, setPageSize] = useState(20);
+  const [sort, setSort] = useState({});
+  const [search, setSearch] = useState("");
+
+  const [searchInput, setSearchInput] = useState("");
+  const { data, isLoading } = useGetTransactionsQuery({
+    page,
+    pageSize,
+    sort: JSON.stringify(sort),
+    search,
+  });
+
+  const columns = [
+    {
+      field: "id",
+      headerName: "id",
+    },
+    {
+      field: "date",
+      headerName: "Date",
+    },
+    {
+      field: "itemName",
+      headerName: "Item Name",
+      flex: 1,
+    },
+    {
+      field: "startDate",
+      headerName: "Booking Date",
+      flex: 1,
+    },
+    {
+      field: "depositeAmount",
+      headerName: "Deposite",
+      flex: 0.5,
+    },
+    {
+      field: "totalAmount",
+      headerName: "Total Amount",
+      flex: 1,
+    },
+    {
+      field: "booking_status",
+      headerName: "Booking Status",
+      flex: 1,
+    },
+    {
+      field: "invoice",
+      headerName: "Invoice",
+      flex: 1,
+    },
+  ];
+
+  const rows = [
+    {
+      id: 1,
+      date: "Jan 2023",
+      itemName: "Scuba Diving",
+      startDate: "15th Jan",
+      depositeAmount: "25%",
+      totalAmount: "2500",
+      booking_status: "Complete",
+      invoice: "Download",
+    },
+    {
+      id: 2,
+      date: "Jan 2023",
+      itemName: "Scuba Diving",
+      startDate: "15th Jan",
+      depositeAmount: "25%",
+      totalAmount: "2500",
+      booking_status: "Complete",
+      invoice: "Download",
+    },
+    {
+      id: 3,
+      date: "Jan 2023",
+      itemName: "Scuba Diving",
+      startDate: "15th Jan",
+      depositeAmount: "25%",
+      totalAmount: "2500",
+      booking_status: "Complete",
+      invoice: "Download",
+    },
+  ];
+
+  return (
+    <Box m="1.5rem 2.5rem">
+      <Header title="Manage Orders" subtitle="Entire list of transactions" />
+      <Box
+        height="80vh"
+        sx={{
+          "& .MuiDataGrid-root": {
+            border: "none",
+          },
+          "& .MuiDataGrid-cell": {
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-columnHeaders": {
+            backgroundColor: theme.palette.background.alt,
+            color: theme.palette.secondary[100],
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-virtualScroller": {
+            backgroundColor: theme.palette.primary.light,
+          },
+          "& .MuiDataGrid-footerContainer": {
+            backgroundColor: theme.palette.background.alt,
+            color: theme.palette.secondary[100],
+            borderTop: "none",
+          },
+          "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+            color: `${theme.palette.secondary[200]} !important`,
+          },
+        }}
+      >
+        <DataGrid
+          getRowId={(rows) => rows.id}
+          rows={rows}
+          columns={columns}
+          rowCount={(data && data.total) || 0}
+          rowsPerPageOptions={[20, 50, 100]}
+          pagination
+          page={page}
+          pageSize={pageSize}
+          paginationMode="server"
+          sortingMode="server"
+          onPageChange={(newPage) => setPage(newPage)}
+          onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
+          onSortModelChange={(newSortModel) => setSort(...newSortModel)}
+          components={{ Toolbar: DataGridCustomToolbar }}
+          componentsProps={{
+            toolbar: { searchInput, setSearchInput, setSearch },
+          }}
+        />
+      </Box>
+    </Box>
+  );
 };
 
 export default ManageOrder;
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import Header from "components/Header";
+import { Box, Button, useTheme } from "@mui/material";
+import FlexBetween from "components/FlexBetween";
 
 const Products = () => {
-  return <div>Products</div>;
+  const theme = useTheme();
+  return (
+    <Box m="1.5rem 2.5rem">
+      <FlexBetween>
+        <Header title="Product" subtitle="Entire list of product" />
+        <Button
+          variant="contained"
+          style={{
+            backgroundColor: theme.palette.secondary.main,
+            color: theme.palette.neutral[600],
+          }}
+        >
+          Add New
+        </Button>
+      </FlexBetween>
+    </Box>
+  );
 };
 
 export default Products;
