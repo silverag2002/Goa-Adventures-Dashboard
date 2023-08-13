6773b6ff568436c5a76facb800976988f158f62d
silverag2002 <silverag2002@gmail.com>
2023-08-13 19:45:57 +0530

    Added terms conditions

--- src/pages/privacy-policy.jsx
+++ src/pages/privacy-policy.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useRef } from "react";
-import { Box, useTheme, Grid } from "@mui/material";
+import { Box, useTheme, Grid, Button, Stack } from "@mui/material";
 import Header from "components/Header";
 import JoditEditor from "jodit-react";
+import Loader from "react-loader";
 
 const PrivacyPolicry = () => {
   const [privacyPolicy, setPrivacyPolicy] = useState("");
@@ -16,6 +17,10 @@ const PrivacyPolicry = () => {
     console.log("Testing acii code", content);
     setPrivacyPolicy(content);
   };
+
+  function handleSubmit() {
+    console.log("Inside handle sibmit", privacyPolicy);
+  }
 
   return (
     <Box m="1.5rem 2.5rem">
@@ -34,6 +39,63 @@ const PrivacyPolicry = () => {
           />
         </Box>
       </Grid>
+
+      <Stack
+        direction="row"
+        justifyContent="flex-end"
+        alignItems="center"
+        spacing={4}
+        sx={{ marginTop: "1rem" }}
+      >
+        <Button
+          size="large"
+          variant="contained"
+          type="submit"
+          style={{
+            backgroundColor: theme.palette.secondary.main,
+            color: theme.palette.neutral[600],
+            fontWeight: "bold",
+          }}
+          onClick={handleSubmit}
+        >
+          Add Terms & Conditions
+        </Button>
+        <Button
+          size="large"
+          variant="contained"
+          style={{
+            backgroundColor: theme.palette.secondary.main,
+            color: theme.palette.neutral[600],
+            fontWeight: "bold",
+          }}
+          href="/products"
+        >
+          Cancel
+        </Button>
+      </Stack>
+      <div className="spinner">
+        <Loader
+          loaded={loaded}
+          lines={13}
+          length={10}
+          width={5}
+          radius={30}
+          corners={1}
+          rotate={0}
+          direction={1}
+          color="#000"
+          speed={1}
+          trail={60}
+          shadow={false}
+          hwaccel={false}
+          className="spinner"
+          zIndex={2e9}
+          top="50%"
+          left="50%"
+          scale={1.0}
+          loadedClassName="loadedContent"
+        />
+      </div>
     </Box>
   );
 };
--- src/pages/terms-conditions.jsx
+++ src/pages/terms-conditions.jsx
@@ -1,10 +1,13 @@
 import React, { useState, useRef } from "react";
-import { Box, useTheme, Grid } from "@mui/material";
+import { Box, useTheme, Grid, Button, Stack } from "@mui/material";
 import Header from "components/Header";
 import JoditEditor from "jodit-react";
+import Loader from "react-loader";
 
 const TermsConditions = () => {
-  const [privacyPolicy, setPrivacyPolicy] = useState("");
+  const [termsConditions, setTermsConditions] = useState("");
+  const theme = useTheme();
+  const [loaded, setLoaded] = useState(true);
   const editor = useRef();
 
   const getSunEditorInstance = (sunEditor) => {
@@ -14,8 +17,12 @@ const TermsConditions = () => {
   const privaacyPolicyHandler = (content) => {
     console.log(" handleEditorChang ", content);
     console.log("Testing acii code", content);
-    setPrivacyPolicy(content);
+    setTermsConditions(content);
   };
+
+  function handleSubmit() {
+    console.log("Inside handle sibmit", termsConditions);
+  }
 
   return (
     <Box m="1.5rem 2.5rem">
@@ -28,12 +35,69 @@ const TermsConditions = () => {
             className="text-black"
             // config={{ theme: "dark" }}
             getSunEditorInstance={getSunEditorInstance}
-            value={privacyPolicy}
+            value={termsConditions}
             onChange={privaacyPolicyHandler}
-            setContents={privacyPolicy}
+            setContents={termsConditions}
           />
         </Box>
       </Grid>
+
+      <Stack
+        direction="row"
+        justifyContent="flex-end"
+        alignItems="center"
+        spacing={4}
+        sx={{ marginTop: "1rem" }}
+      >
+        <Button
+          size="large"
+          variant="contained"
+          type="submit"
+          style={{
+            backgroundColor: theme.palette.secondary.main,
+            color: theme.palette.neutral[600],
+            fontWeight: "bold",
+          }}
+          onClick={handleSubmit}
+        >
+          Add Terms & Conditions
+        </Button>
+        <Button
+          size="large"
+          variant="contained"
+          style={{
+            backgroundColor: theme.palette.secondary.main,
+            color: theme.palette.neutral[600],
+            fontWeight: "bold",
+          }}
+          href="/products"
+        >
+          Cancel
+        </Button>
+      </Stack>
+      <div className="spinner">
+        <Loader
+          loaded={loaded}
+          lines={13}
+          length={10}
+          width={5}
+          radius={30}
+          corners={1}
+          rotate={0}
+          direction={1}
+          color="#000"
+          speed={1}
+          trail={60}
+          shadow={false}
+          hwaccel={false}
+          className="spinner"
+          zIndex={2e9}
+          top="50%"
+          left="50%"
+          scale={1.0}
+          loadedClassName="loadedContent"
+        />
+      </div>
     </Box>
   );
 };
