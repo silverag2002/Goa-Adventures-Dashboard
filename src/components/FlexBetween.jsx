a51a72ca37b3c11580fe4e3839692b3f6531aae5
Ajit Sharma <ajitsharma2021@gmail.com>
2023-09-07 13:37:30 +0530

    Login Form CSS Fixed

--- src/components/FlexBetween.jsx
+++ src/components/FlexBetween.jsx
@@ -5,6 +5,7 @@ const FlexBetween = styled(Box)({
   display: "flex",
   justifyContent: "space-between",
   alignItems: "center",
+  width: "100%",
 });
 
 export default FlexBetween;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -52,99 +52,149 @@ const Login = () => {
   const onError = (errors) => console.log(errors);
 
   return (
-    <Box sx={{ flexGrow: 1, margin: "1.5rem 2.5rem" }}>
-      <Header title="Log In" icon={<ReplyOutlinedIcon />} />
-
-      <form onSubmit={handleSubmit(onSubmit, onError)}>
-        <Grid container spacing={2}>
-          <Grid item xs={12} md={4}>
-            <Box>
-              <Controller
-                name="email"
-                control={control}
-                render={({ field }) => (
-                  <TextField
-                    id="email"
-                    label="Email"
-                    variant="outlined"
-                    fullWidth
-                    margin="normal"
-                    sx={{
-                      backgroundColor: theme.palette.primary.light,
-                    }}
-                    {...field}
-                  />
-                )}
-              />
-            </Box>
-            <Box>
-              <Controller
-                name="password"
-                control={control}
-                render={({ field }) => (
-                  <TextField
-                    id="password"
-                    label="Password"
-                    variant="outlined"
-                    type="password"
-                    fullWidth
-                    margin="normal"
-                    sx={{
-                      backgroundColor: theme.palette.primary.light,
-                    }}
-                    {...field}
-                  />
-                )}
-              />
-            </Box>
-          </Grid>
-        </Grid>
-
-        <Stack
-          direction="row"
-          justifyContent="flex-end"
-          alignItems="center"
-          spacing={4}
-          sx={{ marginTop: "1rem" }}
-        >
-          <Button
-            size="large"
-            variant="contained"
-            type="submit"
-            style={{
-              backgroundColor: theme.palette.secondary.main,
-              color: theme.palette.neutral[600],
-              fontWeight: "bold",
+    <Box
+      sx={{
+        backgroundImage: "url(https://source.unsplash.com/random?landscape)",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        width: "100%",
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Container
+        component="main"
+        maxWidth="xs"
+        sx={{
+          backdropFilter: "blur(25px)",
+          boxShadow: "rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;",
+          borderRadius: "10px",
+        }}
+      >
+        <form onSubmit={handleSubmit(onSubmit, onError)}>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              width: "100%",
+              mt: 2,
             }}
           >
-            Book
-          </Button>
-        </Stack>
+            <Typography
+              variant="h4"
+              sx={{
+                color: theme.palette.neutral.white,
+                fontWeight: "bold",
+                letterSpacing: "0.1rem",
+              }}
+            >
+              LOGIN
+            </Typography>
+            <Controller
+              name="email"
+              control={control}
+              render={({ field }) => (
+                <TextField
+                  id="email"
+                  label="Email"
+                  variant="outlined"
+                  fullWidth
+                  margin="normal"
+                  sx={{
+                    backgroundColor: theme.palette.primary.light,
+                    borderRadius: "0.3rem",
+                    border: "none",
+                    outline: "none",
+                  }}
+                  {...field}
+                />
+              )}
+            />
+            <Controller
+              name="password"
+              control={control}
+              render={({ field }) => (
+                <TextField
+                  id="password"
+                  label="Password"
+                  variant="outlined"
+                  type="password"
+                  fullWidth
+                  margin="normal"
+                  sx={{
+                    backgroundColor: theme.palette.primary.light,
+                    borderRadius: "0.3rem",
+                    border: "none",
+                    outline: "none",
+                  }}
+                  {...field}
+                />
+              )}
+            />
+            <FlexBetween>
+              <FormControlLabel
+                sx={{ color: theme.palette.neutral.white }}
+                control={
+                  <Checkbox
+                    value="remember"
+                    sx={{ color: theme.palette.neutral.white }}
+                  />
+                }
+                label="Remember me"
+              />
+              <Link href="#" variant="body2" sx={{ color: "white" }}>
+                Forget password?
+              </Link>
+            </FlexBetween>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{
+                mt: 2,
+                mb: 2,
+                pt: 1.5,
+                pb: 1.5,
+                backgroundColor: "whitesmoke",
+                color: "black",
+                fontSize: "0.9rem",
+                fontWeight: "bold",
+              }}
+            >
+              Sign In
+            </Button>
+          </Box>
+        </form>
+      </Container>
 
-        <div className="spinner">
-          <Loader
-            loaded={loaded}
-            lines={13}
-            length={20}
-            width={10}
-            radius={30}
-            corners={1}
-            rotate={0}
-            direction={1}
-            color="#000"
-            speed={1}
-            trail={60}
-            shadow={false}
-            hwaccel={false}
-            className="spinner"
-            zIndex={2e9}
-            top="50%"
-            left="50%"
-            scale={1.0}
-            loadedClassName="loadedContent"
-          />
-        </div>
-      </form>
+      <div className="spinner">
+        <Loader
+          loaded={loaded}
+          lines={13}
+          length={20}
+          width={10}
+          radius={30}
+          corners={1}
+          rotate={0}
+          direction={1}
+          color="#000"
+          speed={1}
+          trail={60}
+          shadow={false}
+          hwaccel={false}
+          className="spinner"
+          zIndex={2e9}
+          top="50%"
+          left="50%"
+          scale={1.0}
+          loadedClassName="loadedContent"
+        />
+      </div>
     </Box>
   );
 };
